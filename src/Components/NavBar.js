a4a3877bf791a5ef49ef20583f8a7ac9a8c7702e
nishantmaske2000 <nishantmaske8@gmail.com>
2025-04-12 12:04:23 +0530

    React app converted

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,8 +1,17 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 
 function NavBar() {
     const location = useLocation();
+    const [offCanvasOpen, setOffCanvasOpen] = useState(false);
+
+ useEffect(() => {
+          console.log(location.pathname);
+        }, [location]);
+      
+        const toggleOffCanvas = () => {
+          setOffCanvasOpen(!offCanvasOpen);
+        };
 
     useEffect(() => {
         console.log(location.pathname);
@@ -23,9 +32,28 @@ function NavBar() {
                 <Link to="/contact" className="nav-btn">Join Us</Link>
             </div>
 
-            <div className="menu-toggle">
+            <div className="menu-toggle" onClick={toggleOffCanvas}>
                 <i className="fa-solid fa-bars"></i>
             </div>
+
+            {/* Off-canvas menu */}
+      <div className="off-canvas" style={{ display: offCanvasOpen ? 'flex' : 'none' }}>
+        <li>
+          <Link to="/" onClick={() => setOffCanvasOpen(false)}>Home</Link>
+        </li>
+        <li>
+          <Link to="/about" onClick={() => setOffCanvasOpen(false)}>About Us</Link>
+        </li>
+        <li>
+          <Link to="/program" onClick={() => setOffCanvasOpen(false)}>Programs</Link>
+        </li>
+        <li>
+          <Link to="/gallery" onClick={() => setOffCanvasOpen(false)}>Gallery</Link>
+        </li>
+        <li>
+          <Link to="/contact" onClick={() => setOffCanvasOpen(false)}>Contact</Link>
+        </li>
+      </div>
         </div>
     );
 }
